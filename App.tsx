bce0af624f5f6478dcc2b938bde97bdbabc16a2d
C_Minor <luca.campa.94@gmail.com>
2018-05-27 15:12:14 +0200

    Setup authentication page

--- App.tsx
+++ App.tsx
@@ -35,7 +35,7 @@ const Routes = StackNavigator({
   Authentication: {
     screen: Authentication,
     navigationOptions: {
-
+      title: 'Authenticate'
     }
   }
 });
--- pages/Authentication.tsx
+++ pages/Authentication.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Container, Text } from 'native-base';
+import { Container, Text, View, Item, Input, Icon, Button } from 'native-base';
 import { NavigationScreenProp } from "react-navigation";
+import { StyleSheet } from 'react-native';
 
 interface State {
-
+    email: string;
+    password: string;
+    isAuthenticating: boolean;
+    passwordReference?: any;
 }
 
 interface Props {
@@ -12,11 +16,57 @@ interface Props {
 
 export class Authentication extends React.Component<Props, State> {
 
+    constructor(props: Props){
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            isAuthenticating: false
+        };
+    }
+
     render(){
         return (
-            <Container>
-                <Text>New Page</Text>
+            <Container style={styles.container}>
+                <View style={styles.logo} />
+                <Item style={styles.input}>
+                    <Icon type="Ionicons" name="mail" />
+                    <Input
+                        keyboardType="email-address"
+                        placeholder="Email"
+                        returnKeyType="next"
+                        onChangeText={email => this.setState({...this.state, email})}
+                    />
+                </Item>
+                <Item style={styles.input}>
+                    <Icon type="Ionicons" name="key" />
+                    <Input
+                        placeholder="Password"
+                        secureTextEntry
+                        returnKeyType="done"
+                        autoCorrect={false}
+                        onChangeText={password => this.setState({...this.state, password})}
+                    />
+                </Item>
+                <Button block style={styles.loginButton} >
+                    <Text>Login</Text>
+                </Button>
             </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20
+    },
+    logo: {
+        height: 100
+    },
+    input: {
+        borderColor: 'black'
+    },
+    loginButton: {
+        marginTop: 50
+    }
+});
